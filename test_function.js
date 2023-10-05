76aa9539195f801605dce4a0c8f9021add07fca0
piyavach <111841685+piyavach@users.noreply.github.com>
2023-10-05 08:23:37 +0700

    log file

--- test_function.js
+++ test_function.js
@@ -1,9 +1,13 @@
 const { helloworld, one_plus } = require("./Utils");
+const fs = require('fs');
+
+let str_log = "";
 
 if(helloworld() === "hello world")
 {
 
 }else{
+    str_log = str_log + "helloworld()\n";
     console.log(1);
     return;
 }
@@ -13,6 +17,7 @@ if(one_plus(1) === 2) //oneplus <--- 1 ==> 2
 {
     
 }else{
+    str_log = str_log + "one_plus():1\n";
     console.log(1);
     return;
 }
@@ -22,6 +27,10 @@ if(one_plus(2) === 3) //oneplus <--- 1 ==> 2
 {
     
 }else{
+    str_log = str_log + "one_plus():2\n";
     console.log(1);
     return;
 }
+
+
+fs.writeFileSync('log.txt', str_log);
