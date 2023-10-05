b0149adb7eb3a3b71bb66717f1324a89768e6a5f
piyavach <111841685+piyavach@users.noreply.github.com>
2023-10-05 08:57:45 +0700

    1

--- test_function.js
+++ test_function.js
@@ -1,14 +1,13 @@
-const { helloworld, one_plus } = require("./Utils");
-const fs = require('fs');
 
-let str_log = "";
+
+const { helloworld, one_plus } = require("./Utils");
 
 if(helloworld() === "hello world")
 {
 
 }else{
-    str_log = str_log + "error at helloworld():1\n";
     console.log(1);
+    return;
 }
 
 //test case: one_plus 
@@ -16,9 +15,8 @@ if(one_plus(1) === 2) //oneplus <--- 1 ==> 2
 {
     
 }else{
-    str_log = str_log + "error at one_plus():1\n";
     console.log(1);
-    
+    return;
 }
 
 
@@ -26,10 +24,7 @@ if(one_plus(2) === 3) //oneplus <--- 1 ==> 2
 {
     
 }else{
-    str_log = str_log + "error at one_plus():2\n";
     console.log(1);
+    return;
 }
 
-
-fs.writeFileSync('log.txt', str_log);
-return;
